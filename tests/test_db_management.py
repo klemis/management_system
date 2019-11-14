afb5a15e489cd6433ac04dcbf0fad9e90eb87916
Kamil <kamil.lemisz@gmail.com>
2019-11-14 15:00:47 +0100

    Add test file for db management

---
+++ tests/test_db_management.py
@@ -0,0 +1,17 @@
+"""
+Unit tests for DatabaseOperation class
+"""
+
+from db_management import DatabaseOperation
+
+
+class TestDatabaseOperation:
+
+    def test_create_connection(self):
+        pass
+
+    def test_create_connection(self):
+        pass
+
+    def test_create_connection(self):
+        pass
